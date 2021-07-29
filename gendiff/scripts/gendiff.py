10a63b85019a7d1d5f94d42bfdeaf119fc16dbda
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2021-07-29 17:46:59 +0300

    Add plain diff (json)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
 import argparse
+
+from gendiff import generate_diff
+
 
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('-f', '--format',
@@ -7,3 +11,12 @@ parser.add_argument('first_file')
 parser.add_argument('second_file')
 
 args = parser.parse_args()
+
+
+def main():
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
+
+
+if __name__ == '__main__':
+    main()
