1f13366130dfaf79d2033ef0400778f58bb5363a
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2021-09-02 15:48:23 +0300

    add plain diff representation

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -2,22 +2,43 @@
 from gendiff import parser
 
 
+def make_tree(data1, data2):
+    diff = []
+    diff_keys = list(data1.keys() | data2.keys())
+    diff_keys.sort()
+
+    for key in diff_keys:
+        value1 = data1.get(key)
+        value2 = data2.get(key)
+        diff.append((key, value1, value2))
+
+    return diff
+
+
+def stylish(tree):
+    diff = ['{']
+
+    for node in tree:
+        key, value1, value2 = node
+        if value1 is None:
+            diff.append('  + {}: {}'.format(key, value2))
+        elif value2 is None:
+            diff.append('  - {}: {}'.format(key, value1))
+        elif value1 == value2:
+            diff.append('    {}: {}'.format(key, value1))
+        else:
+            diff.append('  - {}: {}'.format(key, value1))
+            diff.append('  + {}: {}'.format(key, value2))
+
+    diff.append('}')
+
+    return '\n'.join(diff)
+
+
 def generate_diff(file_path1, file_path2):
     data1 = parser.load_data(file_path1)
     data2 = parser.load_data(file_path2)
 
-    diff = ['{']
-    diff_keys = list(data1.keys() | data2.keys())
-    diff_keys.sort()
-    for key in diff_keys:
-        if key not in data1:
-            diff.append('  + {}: {}'.format(key, data2[key]))
-        elif key not in data2:
-            diff.append('  - {}: {}'.format(key, data1[key]))
-        elif data1[key] == data2[key]:
-            diff.append('    {}: {}'.format(key, data1[key]))
-        else:
-            diff.append('  - {}: {}'.format(key, data1[key]))
-            diff.append('  + {}: {}'.format(key, data2[key]))
-    diff.append('}')
-    return '\n'.join(diff)
+    tree = make_tree(data1, data2)
+    tree = stylish(tree)
+    return tree
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,21 @@
-from gendiff import generator
+from gendiff import generator, parser
+
+
+def test_make_plain_tree():
+	file_path1 = 'tests/fixtures/file1.json'
+	file_path2 = 'tests/fixtures/file2.json'
+	data1 = parser.load_data(file_path1)
+	data2 = parser.load_data(file_path2)
+
+	diff_repr = [
+	('follow', False, None), 
+	('host', 'hexlet.io', 'hexlet.io'), 
+	('proxy', '123.234.53.22', None), 
+	('timeout', 50, 20), 
+	('verbose', None, True)
+	]
+
+	assert generator.make_tree(data1, data2) == diff_repr
 
 
 def test_plain_json():
